b3f632dc705e8699cadccec21ace3b9ce0fa56d7
ArminMA <majaris@kth.se>
2016-12-21 14:30:28 +0100

    try to make the app responsiv!

--- src/main/java/org/kth/controller/beans/LoginBean.java
+++ src/main/java/org/kth/controller/beans/LoginBean.java
@@ -1,5 +1,9 @@
 package org.kth.controller.beans;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.kth.controller.handlers.CookieManager;
 import org.kth.controller.handlers.UserHandler;
 import org.kth.model.pojo.UserPojo;
@@ -12,32 +16,67 @@ import java.io.Serializable;
 @ViewScoped
 public class LoginBean implements Serializable {
 
-    UserPojo userPojo;
+    @Size(min=1, max=50)
+    @NotEmpty
+    private String username;
+
+    @Size(min=1, max=50)
+    @NotEmpty
+    private String password;
+
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
     public LoginBean() {
-        userPojo = new UserPojo();
-    }
 
-    public UserPojo getUserPojo() {
-        return userPojo;
-    }
-
-    public void setUserPojo(UserPojo userPojo) {
-        this.userPojo = userPojo;
     }
 
     //controller methods
     public String doLogin() {
-        System.out.println("found doLogin");
+
+
         boolean loginOK = false;
-        if (userPojo != null){
-            if(userPojo.getUsername() != null && userPojo.getPassword() != null ){
-                if(loginOK = UserHandler.login(userPojo))
-                    CookieManager.addUserName(userPojo.getUsername());
-                return "viewPersonalMessages?faces-redirect=true";
-            }
+        if(loginOK = UserHandler.login(new UserPojo(username, null, password))) {
+            CookieManager.addUserName(username);
+            return "viewPersonalMessages?faces-redirect=true";
         }
 
+
         return "index?faces-redirect=true";
+    }
+
+    public void forgotPassword() {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "don't forget your UserName");
+        FacesContext.getCurrentInstance().addMessage("loginForm:username", msg);
+        msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "don't forget your password");
+        FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
+    }
+
+    public void login() {
+        if ("BootsFaces".equalsIgnoreCase(username) && "rocks!".equalsIgnoreCase(password)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Congratulations! You've successfully logged in.");
+            FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
+
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "That's the wrong password.");
+            FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
+        }
     }
 }
